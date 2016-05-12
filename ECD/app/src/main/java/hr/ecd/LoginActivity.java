4d699543d586a41fb4ac1ae5ec54ae87b50b75fb
Boris van Norren <boris0101@hotmail.com>
2016-05-12 16:39:24 +0200

    - Added new login test button - Added new LoginActivity

---
+++ ECD/app/src/main/java/hr/ecd/LoginActivity.java
@@ -0,0 +1,15 @@
+package hr.ecd;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Boris_000 on 12-5-2016.
+ */
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
