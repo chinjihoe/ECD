9a68ad7b44e422a56184962898b4f2c539ace072
Chinji Hoe <chinji.hoe@hotmail.com>
2016-05-13 17:01:51 +0200

    Test: speech to text

--- app/src/main/java/hr/ecd/CentralScreenActivity.java
+++ app/src/main/java/hr/ecd/CentralScreenActivity.java
@@ -1,10 +1,13 @@
 package hr.ecd;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 /**
@@ -19,6 +22,13 @@ public class CentralScreenActivity extends AppCompatActivity {
         Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final Button speechButton = (Button)findViewById(R.id.speechButton);
+        speechButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CentralScreenActivity.this,SpeechToTextActivity.class));
+            }
+        });
     }
 
 
