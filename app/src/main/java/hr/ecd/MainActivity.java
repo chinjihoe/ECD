22ed4fbc0a17c8ce47d0e3aac26999e980d0cbcf
Chinji Hoe <chinji.hoe@hotmail.com>
2016-05-13 11:14:16 +0200

    Splashscreen acts as loading screen
    de splashscreen functioneert als een loading screen.
    hoe sneller het geladen is, hoe sneller de splash zal verdwijnen

--- app/src/main/java/hr/ecd/MainActivity.java
+++ app/src/main/java/hr/ecd/MainActivity.java
@@ -1,46 +1,19 @@
 package hr.ecd;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.view.View;
-import java.util.Timer;
-import java.util.TimerTask;
-import android.widget.ImageView;
 
-
-/*
-    De activity die gebeurd aan het begin van de app
+/**
+    Een activity die na de splash gebeurd en voor de login screen
+    Hier kan gecheckt worden of de persoon al ingelogd is zodat de loginscreen overgeslagen kan worden
  */
-
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
 
-        final Animation animationFadeIn = AnimationUtils.loadAnimation(this, R.anim.fadein);
-        final ImageView image = (ImageView)findViewById(R.id.hpimage);
-
-        image.startAnimation(animationFadeIn);
-        image.setRotation(0); //startAnimation is een infinite loop, dus rotation(0) om het maar 1 keer te animeren
-
-
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                startActivity(new Intent(MainActivity.this,LoginActivity.class)); //To login screen
-                finish(); //maakt een einde aan de activity zodat je niet "Back" kan naar deze activiy
-            }
-        }, 2000);
-
-
-
-
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
     }
-
 }
