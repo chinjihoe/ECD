37a99c8b2cd3b2f84ba6f3880a1b699d9dd35594
chinjihoe <chinji.hoe@hotmail.com>
2016-06-02 15:26:56 +0200

    Fixed listview of dossier layout

--- app/src/main/java/hr/ecd/DossierActivity.java
+++ app/src/main/java/hr/ecd/DossierActivity.java
@@ -38,6 +38,7 @@ public class DossierActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         mDrawerList = (ListView)findViewById(R.id.navList);
+
         addDrawerItems();
         setupDrawer();
         drawerListener();
@@ -55,7 +56,16 @@ public class DossierActivity extends AppCompatActivity {
         mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(DossierActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+                mDrawerLayout.closeDrawer(mDrawerList);
+                switch (position){
+                    case 0:
+                        break;
+                    case 1:
+                        startActivity(new Intent(DossierActivity.this,SpeechActivity.class));
+                        break;
+                    default:
+                        break;
+                }
             }
         });
     }
@@ -69,6 +79,8 @@ public class DossierActivity extends AppCompatActivity {
     private void setupDrawer() {
         mActivityTitle = getTitle().toString();
         mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerList.bringToFront();
+        mDrawerLayout.requestLayout();
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
 
             /** Called when a drawer has settled in a completely open state. */
